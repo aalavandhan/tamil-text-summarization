a05225f797080b773d896d7ab84334d5d62f67fd
handigun <handigun@usc.edu>
2016-04-16 17:38:22 -0700

    Update kannada_metho2.py

--- kannada_metho2.py
+++ kannada_metho2.py
@@ -1,49 +1,45 @@
-#!/usr/bin/python
 import sys
-import operator
-def sentrank(argv):
+def kannada_method2(argv):
     path=argv[1]
-    no_of_sentences=int(argv[2])
-    sentence={}
-    ##f1=open("C:/Users/chetan/Desktop/tamil-text-summarization-master/article-1.txt","r",encoding="utf8")
+    no_of_sentences=argv[2]
     f1=open(path,"r",encoding="utf8")
-    s=f1.read()
-    para_lst=s.split("\n")
-    sen_no=1
+    f1=open("C:/Users/chetan/Desktop/tamil-text-summarization-master/article-1.txt","r",encoding="utf8")
     word_dic={}
-    for para in para_lst:
-        para=para.strip()
-        para_pos=1
-        sen_lst=para.split(".")
-        for sen in sen_lst:
-            word_lst=sen.split(" ")
-            for word in word_lst:
-                word=word.strip("\"")
-                word_dic.setdefault(word,0)
-            sentence.setdefault(sen_no,{'position':0,'para_position':0,'length':0,'para_pos_score':0,'pos_score':0,'len_score':0,'surface_score':0,'content':''})
-            sentence[sen_no]['position']=sen_no
-            sentence[sen_no]['para_position']=para_pos
-            sentence[sen_no]['length']=len(word_lst)
-            sentence[sen_no]['content']=sen
-            sen_no+=1
-            para_pos+=1
-        for iter in range(1,para_pos):
-            sentence[iter]['para_pos_score']=1-sentence[iter]['para_position']/para_pos     
-    for iter in range(1,sen_no):
-        sentence[iter]['pos_score']=1-sentence[iter]['position']/sen_no
-        sentence[iter]['len_score']=sentence[iter]['length']/len(word_dic)
-        sentence[iter]['surface_score']=sentence[iter]['pos_score']+sentence[iter]['para_pos_score']+sentence[iter]['len_score']
+    sentence_score={}
+    no=0
+    s=f1.read()
+    f1.close()
+    line_lst=s.split(".")
+    for line in line_lst:
+        word_lst=line.split(" ")
+        no+=len(word_lst)
+        for word in word_lst:
+            word=word.strip(".")
+            word_dic.setdefault(word,0)
+            word_dic[word]+=1
 
     
+    n=1
+    
+    sent_lst=s.split(".")
+    for word in word_dic:
+        word_dic[word]/=no
+    for sent in sent_lst:
+        ##print(sent_lst)
+        sentence_score.setdefault(n,{'score':0,'length':0,'odia_score':0,'content':''})
+        sent=sent.strip()
+        sentence_score[n]['content']=sent
+        word_lst=sent.split(" ")
+        sentence_score[n]['length']=len(word_lst)
+        ##print(word_lst)
+        for word in word_lst:
+            print(n)
+            sentence_score[n]['score']+=word_dic[word]
+            sentence_score[n]['odia_score']+=word_dic[word]/sentence_score[n]['length']
+        n+=1
 
-    sorted_lst=sorted(sentence.items(), key=lambda x: x[1]['surface_score'],reverse=True)
-    for i in range(0,no_of_sentences):
-        print(sentence[sorted_lst[i][0]]['content'])
+
+
+        
 if __name__=="__main__":
-    sentrank(sys.argv)
-
-
-
-'''to do 1) stemming
-    2)Removal (stripping) of ""
-    '''
+    kannada_method2(sys.argv)

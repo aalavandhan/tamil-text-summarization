53fa96e7fbf0f676afceaeac5a6f2000f80c956c
handigun <handigun@usc.edu>
2016-04-16 17:39:25 -0700

    Update method2.py

--- method2.py
+++ method2.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 import sys
+import operator
 def sentrank(argv):
-    ##path=argv[1]
+    path=argv[1]
+    no_of_sentences=int(argv[2])
     sentence={}
-    f1=open("C:/Users/chetan/Desktop/tamil-text-summarization-master/article-1.txt","r",encoding="utf8")
+    ##f1=open("C:/Users/chetan/Desktop/tamil-text-summarization-master/article-1.txt","r",encoding="utf8")
+    f1=open(path,"r",encoding="utf8")
     s=f1.read()
     para_lst=s.split("\n")
     sen_no=1
@@ -15,11 +18,13 @@ def sentrank(argv):
         for sen in sen_lst:
             word_lst=sen.split(" ")
             for word in word_lst:
+                word=word.strip("\"")
                 word_dic.setdefault(word,0)
-            sentence.setdefault(sen_no,{'position':0,'para_position':0,'length':0,'para_pos_score':0,'pos_score':0,'len_score':0,'surface_score':0})
+            sentence.setdefault(sen_no,{'position':0,'para_position':0,'length':0,'para_pos_score':0,'pos_score':0,'len_score':0,'surface_score':0,'content':''})
             sentence[sen_no]['position']=sen_no
             sentence[sen_no]['para_position']=para_pos
             sentence[sen_no]['length']=len(word_lst)
+            sentence[sen_no]['content']=sen
             sen_no+=1
             para_pos+=1
         for iter in range(1,para_pos):
@@ -29,9 +34,16 @@ def sentrank(argv):
         sentence[iter]['len_score']=sentence[iter]['length']/len(word_dic)
         sentence[iter]['surface_score']=sentence[iter]['pos_score']+sentence[iter]['para_pos_score']+sentence[iter]['len_score']
 
+    
 
+    sorted_lst=sorted(sentence.items(), key=lambda x: x[1]['surface_score'],reverse=True)
+    for i in range(0,no_of_sentences):
+        print(sentence[sorted_lst[i][0]]['content'])
 if __name__=="__main__":
     sentrank(sys.argv)
 
-#to do : stemming
-#haven't checked for correctness
+
+
+'''to do 1) stemming
+    2)Removal (stripping) of ""
+    '''

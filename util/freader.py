5df35e7e042f0fa536a1e55490c5e78351d65ea3
Nithin Krishna <nithinkrishh@gmail.com>
2016-04-27 03:58:27 -0700

    additional reading functions

--- util/freader.py
+++ util/freader.py
@@ -9,3 +9,13 @@ class FileReader:
 
   def stop(self):
     self.inputFile.close()
+
+
+def readAsString(filePath):
+  return "\n".join(readAsList(filePath))
+
+def readAsList(filePath):
+  r = FileReader(filePath)
+  res = [ ]
+  r.read(lambda l: res.append(l))
+  return res
